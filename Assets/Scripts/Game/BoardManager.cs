f937f163a9a7836c55fb4ab6bbc8b6420e497e6e
ad-qu <adquiros15@outlook.com>
2022-06-24 16:27:36 +0200

    New prefabs & more detailed design of levels

--- Assets/Scripts/Game/BoardManager.cs
+++ Assets/Scripts/Game/BoardManager.cs
@@ -21,18 +21,19 @@ public class BoardManager : MonoBehaviour
     public int rows = 10;
     public int columns = 20;
 
+    public GameObject coin, basicGoomba, kingGoomba;
 
-    public GameObject sky, ground, basicWall, brick;
+    public GameObject sky, ground, basicWall, brick, invisibleBlock;
 
     public GameObject[] wall, smallBush, bigBush, smallCloud;
 
-    int i, y;
+    int i = -20, y;
 
     public void SetupScene(int level)
     {
         if (level == 1)
         {
-            while (i < 22)
+            while (i < 42)
             {
                 while (y < 15)
                 {
@@ -44,27 +45,53 @@ public class BoardManager : MonoBehaviour
                 i++;
             }
 
-            i = 0; y = -1;
+            i = -20; y = -1;
 
             while (y < 1)
             {
-                while (i < 22)
+                while (i < 42)
                 {
                     Instantiate(ground, new Vector3(i, y), Quaternion.identity);
                     i++;
                 }
 
-                i = 0;
+                i = -42;
                 y++;
             }
 
+            Instantiate(invisibleBlock, new Vector3(2, 1), Quaternion.identity);
+            Instantiate(invisibleBlock, new Vector3(2, 2), Quaternion.identity);
+            Instantiate(invisibleBlock, new Vector3(2, 3), Quaternion.identity);
+            Instantiate(invisibleBlock, new Vector3(2, 4), Quaternion.identity);
+            Instantiate(invisibleBlock, new Vector3(2, 5), Quaternion.identity);
+
+            Instantiate(smallBush[0], new Vector3(-5, 1), Quaternion.identity);
+            Instantiate(smallBush[1], new Vector3(-4, 1), Quaternion.identity);
+
             Instantiate(smallBush[0], new Vector3(2, 1), Quaternion.identity);
             Instantiate(smallBush[1], new Vector3(3, 1), Quaternion.identity);
+
+            Instantiate(smallBush[0], new Vector3(2, 1), Quaternion.identity);
+            Instantiate(smallBush[1], new Vector3(3, 1), Quaternion.identity);
+
+            Instantiate(brick, new Vector3(15, 4), Quaternion.identity);
+            Instantiate(brick, new Vector3(16, 4), Quaternion.identity);
+            Instantiate(brick, new Vector3(17, 4), Quaternion.identity);
+
+            Instantiate(coin, new Vector3(16, 5), Quaternion.identity);
+
+            Instantiate(basicGoomba, new Vector3(14, 1), Quaternion.identity);
+            Instantiate(kingGoomba, new Vector3(16, 1), Quaternion.identity);
 
             Instantiate(smallCloud[2], new Vector3(4, 5), Quaternion.identity);
             Instantiate(smallCloud[0], new Vector3(4, 6), Quaternion.identity);
             Instantiate(smallCloud[3], new Vector3(5, 5), Quaternion.identity);
             Instantiate(smallCloud[1], new Vector3(5, 6), Quaternion.identity);
+
+            Instantiate(smallCloud[2], new Vector3(16, 7), Quaternion.identity);
+            Instantiate(smallCloud[0], new Vector3(16, 8), Quaternion.identity);
+            Instantiate(smallCloud[3], new Vector3(17, 7), Quaternion.identity);
+            Instantiate(smallCloud[1], new Vector3(17, 8), Quaternion.identity);
 
             Instantiate(wall[0], new Vector3(10, 1), Quaternion.identity);
             Instantiate(wall[1], new Vector3(10, 2), Quaternion.identity);
